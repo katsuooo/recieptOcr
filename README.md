e4bb650c2a069cf6495d5c5c05aad4b6ca675858
katsuooo <katsuooo.g@gmail.com>
2023-03-26 17:29:05 +0900

    test read success

--- README.md
+++ README.md
@@ -3,6 +3,32 @@ text parse from reciept picture
 
 
 
-pyocr
-tesseract
+pyocr / pip install
+tesseract / windows installer tesseract-ocr-w64-setup-5.3.0.20221222.exe
+インストーラ実行時
+choose componentで
+select omponents to install　で
+japanese script
+japanese vertical script
+を選択(verticalは縦書き用)
+//--
+install後
+C:\program files\tesseract-OCR\tessdata\scriptから
+japanese.traineddata
+japanese_vert.traineddata
+をC:\program files\tesseract-OCR\tessdataにコピー
+
+パスを２つ追加
+C:\program files\tesseract-OCR
+C:\program files\tesseract-OCR\tessdata
+
+
+これで実行できたが、画像が荒いと変換もあらい
+
+tesseract_layout=6
+
+で調整できるらしい
+
+
+
 
--- main.py
+++ main.py
@@ -4,3 +4,37 @@
 #
 #
 #
+
+
+#
+# referene code
+#
+import os
+from PIL import Image
+import pyocr
+
+#環境変数「PATH」にTesseract-OCRのパスを設定。
+#Windowsの環境変数に設定している場合は不要。
+#path='C:\\Program Files\\Tesseract-OCR\\'
+#os.environ['PATH'] = os.environ['PATH'] + path
+#path='C:/Program Files/Tesseract-OCR/tessdata/'
+#os.environ['PATH'] = os.environ['PATH'] + path
+#pyocrにTesseractを指定する。
+pyocr.tesseract.TESSERACT_CMD = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#pyocr.tesseract.TESSERACT_CMD = 'C:\Program Files\Tesseract-OCR\tesseract.exe'
+tools = pyocr.get_available_tools()
+tool = tools[0]
+
+#文字を抽出したい画像のパスを選ぶ
+#img = Image.open('画像のパス/画像の名前.JPG')
+img = Image.open('./test-pic\\IMG20230326153606.jpg')
+
+#画像の文字を抽出
+builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+#text = tool.image_to_string(img, lang="jpn", builder=builder)
+text = tool.image_to_string(img, lang="japanese", builder=builder)
+
+print(text)
+
+with open('result.txt','w',encoding='utf-8') as f:
+    f.write(text)
